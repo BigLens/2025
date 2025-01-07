5c4817166d9b10c6c2e7a7d1e2d58f6dbe679995
Biglens <adedejibioku@gmail.com>
2025-01-07 19:28:19 +0100

    updating business logic

--- everythingNest/to-do/src/auth/auth.service.ts
+++ everythingNest/to-do/src/auth/auth.service.ts
@@ -1,7 +1,22 @@
+import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from '../dto';
+import { Injectable } from '@nestjs/common';
+import { response } from 'express';
 
+@Injectable()
 export class AuthService {
-  createTask(dto: AuthDto) {
-    return { msg: 'task created' };
+  constructor(private prisma: PrismaService){}
+  async createTask(dto: AuthDto) {
+   try {
+    //create a user
+    const user = await this.prisma.user.create({
+      data:{
+        email: dto.email,
+      }
+    })
+    
+   } catch (error) {
+    
+   }
   }
 }
