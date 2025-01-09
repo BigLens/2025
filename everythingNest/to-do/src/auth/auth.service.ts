3973e527e6b4202081a4d91c63c170772815250f
Biglens <adedejibioku@gmail.com>
2025-01-09 15:28:46 +0100

    signin handled

--- everythingNest/to-do/src/auth/auth.service.ts
+++ everythingNest/to-do/src/auth/auth.service.ts
@@ -9,13 +9,16 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 export class AuthService {
   constructor(private prisma: PrismaService){}
   async signup(dto: AuthDto) {
+    if(!dto.email || dto.email.trim() === ''){
+      throw new ForbiddenException('email cannot be empty')
+    }
     const checkIfUserExist = await this.prisma.user.findUnique({
       where: {email: dto.email}
     })
     if(checkIfUserExist){
       throw new ForbiddenException('the details is not available for use, choose another!')
     }
-    
+
    try {
     const hash = await argon.hash(dto.password)
     const user = await this.prisma.user.create({
@@ -35,5 +38,20 @@ export class AuthService {
     }
     throw error;
    }
+  }
+
+  async signin(dto: AuthDto){
+    //verify user
+    const verifyUserSignin = await this.prisma.user.findUnique({
+      where: {email: dto.email}
+    })
+    if(!verifyUserSignin){
+      throw new ForbiddenException('user not found')
+    }
+    //check password
+    const pswd = await argon.verify(verifyUserSignin.hash, dto.password);
+    if(!pswd){
+      throw new ForbiddenException('invalid password')
+    }
   }
 }
