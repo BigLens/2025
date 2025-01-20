2cd007e4914711637ef40914bd4da1efcae38beb
Biglens <adedejibioku@gmail.com>
2025-01-20 15:38:10 +0100

    startegy done

--- everythingNest/to-do/src/auth/guard/auth.guard.ts
+++ everythingNest/to-do/src/auth/guard/auth.guard.ts
@@ -1,13 +1,5 @@
-import { ExecutionContext, Injectable } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt'){
-    async canActivate(context: ExecutionContext): Promise<boolean> {
-        // Call the default guard logic
-        const result = (await super.canActivate(context)) as boolean;
-    
-        // Add additional logic if needed (e.g., logging or permissions)
-        return result;
-}
-}
+export class JwtAuthGuard extends AuthGuard('jwt'){}
