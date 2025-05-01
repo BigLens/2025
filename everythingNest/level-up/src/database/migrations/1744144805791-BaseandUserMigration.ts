338a6f2ea7d89249953dcb04a043fabc4ffe713f
Biglens <adedejibioku@gmail.com>
2025-05-01 19:13:38 +0100

    migration files included in the linting fix

--- everythingNest/level-up/src/database/migrations/1744144805791-BaseandUserMigration.ts
+++ everythingNest/level-up/src/database/migrations/1744144805791-BaseandUserMigration.ts
@@ -1,19 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class BaseandUserMigration1744144805791 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             CREATE TABLE "user" (
               "id" SERIAL PRIMARY KEY,
               "email" VARCHAR NOT NULL,
               "password" VARCHAR NOT NULL,
             );
           `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "user"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`);
+  }
 }
