6f9e27f8dc9d8fb834e9be2ce1aea517f98f052f
Biglens <adedejibioku@gmail.com>
2025-05-14 19:12:19 +0100

    migration error fixed

--- everythingNest/level-up/src/database/migrations/1744144805791-BaseandUserMigration.ts
+++ everythingNest/level-up/src/database/migrations/1744144805791-BaseandUserMigration.ts
@@ -3,13 +3,13 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class BaseandUserMigration1744144805791 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            CREATE TABLE "user" (
+      CREATE TABLE "user" (
               "id" SERIAL PRIMARY KEY,
               "email" VARCHAR NOT NULL,
-              "password" VARCHAR NOT NULL,
-            );
-          `);
-  }
+              "password" VARCHAR NOT NULL
+              );
+              `);
+            }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP TABLE "user"`);
