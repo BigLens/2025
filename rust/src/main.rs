39b223b7878f504786eeea7a246edfd7548e2384
BigLens <adedejibioku@gmail.com>
2025-06-03 23:33:53 +0100

    commited changes

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,6 +6,6 @@ fn main(){
     let message = String::from("Hello fellow Rustaceans");
     let width = message.chars().count();
     
-    let mut writer = BufWriter::new(stdout.lock());
+    const mut writer = BufWriter::new(stdout.lock());
     say(&message, width, &mut writer).unwrap();
 }
